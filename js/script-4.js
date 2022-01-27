2ea1fb5ecceef1bee6b9d1b97b07c413df633273
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-01-27 14:07:48 +0200

    practic

--- js/script-4.js
+++ js/script-4.js
@@ -224,73 +224,175 @@
 // };
 // console.log(substitute([58, 14, 48, 12, 31, 19, 10]));
 
-const users = [
-  {
-    name: "Moore Hensley",
-    email: "moorehensley@indexia.com",
-    eyeColor: "blue",
-    friends: ["Sharron Pace"],
-    isActive: false,
-    balance: 2811,
-    gender: "male",
-  },
-  {
-    name: "Sharlene Bush",
-    email: "sharlenebush@tubesys.com",
-    eyeColor: "blue",
-    friends: ["Briana Decker", "Sharron Pace"],
-    isActive: true,
-    balance: 3821,
-    gender: "female",
-  },
-  {
-    name: "Ross Vazquez",
-    email: "rossvazquez@xinware.com",
-    eyeColor: "green",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-    isActive: false,
-    balance: 3793,
-    gender: "male",
-  },
-  {
-    name: "Elma Head",
-    email: "elmahead@omatom.com",
-    eyeColor: "green",
-    friends: ["Goldie Gentry", "Aisha Tran"],
-    isActive: true,
-    balance: 2278,
-    gender: "female",
-  },
-  {
-    name: "Carey Barr",
-    email: "careybarr@nurali.com",
-    eyeColor: "blue",
-    friends: ["Jordan Sampson", "Eddie Strong"],
-    isActive: true,
-    balance: 3951,
-    gender: "male",
-  },
-  {
-    name: "Blackburn Dotson",
-    email: "blackburndotson@furnigeer.com",
-    eyeColor: "brown",
-    friends: ["Jacklyn Lucas", "Linda Chapman"],
-    isActive: false,
-    balance: 1498,
-    gender: "male",
-  },
-  {
-    name: "Sheree Anthony",
-    email: "shereeanthony@kog.com",
-    eyeColor: "brown",
-    friends: ["Goldie Gentry", "Briana Decker"],
-    isActive: true,
-    balance: 2764,
-    gender: "female",
-  },
-];
-const getUsersWithEyeColor = (users, color) => {
-  const arrUsers = users.filter((user) => user.eyeColor === color);
-  return arrUsers;
-};
-console.log(getUsersWithEyeColor(users, "blue"));
+// const users = [
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     gender: "male",
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     gender: "female",
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     gender: "male",
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     gender: "female",
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     isActive: true,
+//     balance: 3951,
+//     gender: "male",
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     isActive: false,
+//     balance: 1498,
+//     gender: "male",
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     gender: "female",
+//   },
+// ];
+// const getUsersWithEyeColor = (users, color) => {
+//   const arrUsers = users.filter((user) => user.eyeColor === color);
+//   return arrUsers;
+// };
+// console.log(getUsersWithEyeColor(users, "blue"));
+
+// =================================================================
+// 26.01.2022
+
+// Write a function - positiveSum
+// It should accept an array of numbers and return a result of their addition.
+// But you must calculate only positive numbers and omit negative if any presents.
+// For example:
+// positiveSum([2, 4, 6, 8])   // => 20
+// positiveSum([0, -3, 5, 7])  // => 12
+
+// const positiveSum = (array) => {
+//   const newArr = array.filter((item) => item >= 0);
+//   return newArr.reduce((acc, arg) => {
+//     return acc + arg;
+//   }, 0);
+// };
+
+// console.log(positiveSum([2, 4, 6, 8])); // => 20
+// console.log(positiveSum([0, -3, 5, 7])); // => 12
+
+// const positiveSum = (args) => {
+//   return args.reduce((acc, element) => {
+//     if (element >= 0) {
+//       return acc + element;
+//     } else {
+//       return acc;
+//     }
+//   }, 0);
+// };
+
+// console.log(positiveSum([2, 4, 6, 8])); // => 20
+// console.log(positiveSum([0, -3, 5, 7])); // => 12
+
+//  Написать функцию — letterCount
+// Он принимает два строковых аргумента и возвращает целое число, соответствующее количеству вхождений второго аргумента в первом.
+// Если вхождений не найдено, должно быть возвращено значение 0.
+// Например:
+// letterCount("Maggy", "g") // => 2
+// letterCount("Барри", "b") // => 1
+// количество букв("", "z") // => 0
+
+// const letterCount = (string1, string2) => {
+//   const firstArray = [...string1.toLowerCase()];
+//   const secondString = string2.toLowerCase();
+//   const filteredArr = firstArray.flter {
+//     ((element) => element === secondString);
+// }
+//   const result = filteredArr.length;
+//   return result;
+// };
+
+// letterCount("Maggy", "g"); // => 2
+// letterCount("Barry", "b"); // => 1
+// letterCount("", "z"); // => 0
+
+// const letterCount = (string1, string2) => {
+//   const firstArray = [...string1.toLowerCase()];
+//   const secondString = string2.toLowerCase();
+//   return firstArray.reduce((total, element) => {
+//     if (element === secondString) {
+//       return total + 1;
+//     }
+//     return total;
+//   }, 0);
+// };
+
+// letterCount("Maggy", "g"); // => 2
+// letterCount("Barry", "b"); // => 1
+// letterCount("", "z"); // => 0
+
+// Our football team completed the championship. The result of each match look like "x:y".
+// Results of all matches are recorded in the collection like this: ["3:1", "2:2", "0:1", ...]
+
+// Write a function – countPoints
+// It should accept a collection of football games scores and count the points of our team in the championship.
+// Rules for counting points for each match:
+// •	if x > y   - 3 points
+// •	if x < y   - 0 point
+// •	if x = y   - 1 point
+
+// For example:
+// countPoints(['3:1', '1:0', '0:0', '1:2', '4:0', '2:3', '1:1', '0:1', '2:1', '1:0']) // => 17
+// countPoints(['1:1', '1:2', '2:0', '4:2', '0:1', '2:3', '1:1', '0:1', '1:1', '3:0']) // => 12
+
+// const countPoints = (array) =>
+//   array.reduce((acc, elem) => {
+//     if (elem[0] > elem[2]) {
+//       return acc + 3;
+//     } else if (elem[0] === elem[2]) {
+//       return acc + 1;
+//     }
+//     return acc;
+//   }, 0);
+
+// console.log(countPoints(["3:1", "1:0", "0:0", "1:2", "4:0", "2:3", "1:1", "0:1", "2:1", "1:0"])); // => 17
+// countPoints(["1:1", "1:2", "2:0", "4:2", "0:1", "2:3", "1:1", "0:1", "1:1", "3:0"]); // => 12
+
+// getSums([1, 2, 3, 4, 5]);
+
+const getSums = (array) => console.log(getSums([1, 2, 3, 4, 5]));
+// должна вернуть - [1, 3, 6, 10, 15]
