32fea16dbfe080b104084663dd7e798e7fe046e5
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-01-31 14:03:03 +0200

    practic

--- js/script-4.js
+++ js/script-4.js
@@ -557,15 +557,37 @@ const users = [
 
 // const newArray = users.map(({ name, gender, age }) => ({ name, gender, age }));
 // console.log(newArray);
-const showThis = () => {
-  console.log("this in showThis: ", this);
-};
+// const showThis = () => {
+//   console.log("this in showThis: ", this);
+// };
 
-showThis(); // this in showThis: window
+// showThis(); // this in showThis: window
 
-const user = {
-  username: "Mango",
-};
-user.showContext = showThis;
+// const user = {
+//   username: "Mango",
+// };
+// user.showContext = showThis;
 
-user.showContext(); // this in showThis: window
+// user.showContext(); // this in showThis: window
+
+class Car {
+  static #MAX_PRICE = 50000;
+  // Change code below this line
+  checkPrice(price) {
+    if (price > MAX_PRICE) {
+      return "Error! Price exceeds the maximum";
+    } else {
+      return "Success! Price is within acceptable limits";
+    }
+  }
+  // Change code above this line
+  constructor({ price }) {
+    this.price = price;
+  }
+}
+
+const audi = new Car({ price: 36000 });
+const bmw = new Car({ price: 64000 });
+
+console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
