600c0c174729c4e4dc51a6330e9f3fd084041dbe
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-02-01 22:31:46 +0200

    add new file

--- js/script-4.js
+++ js/script-4.js
@@ -726,15 +726,3 @@
 // // }
 // // const school = new FrontendSchool(listOfStudents);
 // // school.showStudents();
-
-const message = document.querySelector("#message");
-console.log(message.value); // Default textarea message
-
-//===================================================
-const activeLink = document.querySelector(".btn.active");
-console.log(activeLink.href); // https://s.codepen.io/about
-
-//===================================================
-const image = document.querySelector(".image");
-console.log(image.src); // https://placeimg.com/640/480/animals
-image.src = "https://placeimg.com/640/480/tech";
