8006dd878f5fac4cc46b125028ede9bce77f09ca
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-02-01 13:59:21 +0200

    test

--- js/script-4.js
+++ js/script-4.js
@@ -445,92 +445,92 @@
 // const getSums = (array) => console.log(getSums([1, 2, 3, 4, 5]));
 // должна вернуть - [1, 3, 6, 10, 15]
 
-const users = [
-  {
-    id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
-    name: "Moore Hensley",
-    email: "moorehensley@indexia.com",
-    eyeColor: "blue",
-    friends: ["Sharron Pace"],
-    isActive: false,
-    balance: 2811,
-    skills: ["ipsum", "lorem"],
-    gender: "male",
-    age: 37,
-  },
-  {
-    id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
-    name: "Sharlene Bush",
-    email: "sharlenebush@tubesys.com",
-    eyeColor: "blue",
-    friends: ["Briana Decker", "Sharron Pace"],
-    isActive: true,
-    balance: 3821,
-    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
-    gender: "female",
-    age: 34,
-  },
-  {
-    id: "88beb2f3-e4c2-49f3-a0a0-ecf957a95af3",
-    name: "Ross Vazquez",
-    email: "rossvazquez@xinware.com",
-    eyeColor: "green",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-    isActive: false,
-    balance: 3793,
-    skills: ["nulla", "anim", "proident", "ipsum", "elit"],
-    gender: "male",
-    age: 24,
-  },
-  {
-    id: "249b6175-5c30-44c6-b154-f120923736f5",
-    name: "Elma Head",
-    email: "elmahead@omatom.com",
-    eyeColor: "green",
-    friends: ["Goldie Gentry", "Aisha Tran"],
-    isActive: true,
-    balance: 2278,
-    skills: ["adipisicing", "irure", "velit"],
-    gender: "female",
-    age: 21,
-  },
-  {
-    id: "334f8cb3-eb04-45e6-abf4-4935dd439b70",
-    name: "Carey Barr",
-    email: "careybarr@nurali.com",
-    eyeColor: "blue",
-    friends: ["Jordan Sampson", "Eddie Strong"],
-    isActive: true,
-    balance: 3951,
-    skills: ["ex", "culpa", "nostrud"],
-    gender: "male",
-    age: 27,
-  },
-  {
-    id: "150b00fb-dd82-427d-9faf-2879ea87c695",
-    name: "Blackburn Dotson",
-    email: "blackburndotson@furnigeer.com",
-    eyeColor: "brown",
-    friends: ["Jacklyn Lucas", "Linda Chapman"],
-    isActive: false,
-    balance: 1498,
-    skills: ["non", "amet", "ipsum"],
-    gender: "male",
-    age: 38,
-  },
-  {
-    id: "e1bf46ab-7168-491e-925e-f01e21394812",
-    name: "Sheree Anthony",
-    email: "shereeanthony@kog.com",
-    eyeColor: "brown",
-    friends: ["Goldie Gentry", "Briana Decker"],
-    isActive: true,
-    balance: 2764,
-    skills: ["lorem", "veniam", "culpa"],
-    gender: "female",
-    age: 39,
-  },
-];
+// const users = [
+//   {
+//     id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     skills: ["ipsum", "lorem"],
+//     gender: "male",
+//     age: 37,
+//   },
+//   {
+//     id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+//     gender: "female",
+//     age: 34,
+//   },
+//   {
+//     id: "88beb2f3-e4c2-49f3-a0a0-ecf957a95af3",
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+//     gender: "male",
+//     age: 24,
+//   },
+//   {
+//     id: "249b6175-5c30-44c6-b154-f120923736f5",
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     skills: ["adipisicing", "irure", "velit"],
+//     gender: "female",
+//     age: 21,
+//   },
+//   {
+//     id: "334f8cb3-eb04-45e6-abf4-4935dd439b70",
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     isActive: true,
+//     balance: 3951,
+//     skills: ["ex", "culpa", "nostrud"],
+//     gender: "male",
+//     age: 27,
+//   },
+//   {
+//     id: "150b00fb-dd82-427d-9faf-2879ea87c695",
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     isActive: false,
+//     balance: 1498,
+//     skills: ["non", "amet", "ipsum"],
+//     gender: "male",
+//     age: 38,
+//   },
+//   {
+//     id: "e1bf46ab-7168-491e-925e-f01e21394812",
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     skills: ["lorem", "veniam", "culpa"],
+//     gender: "female",
+//     age: 39,
+//   },
+// ];
 
 // const allBalance = (arrUsers) => {
 //   const balance = arrUsers
@@ -570,24 +570,171 @@ const users = [
 
 // user.showContext(); // this in showThis: window
 
-class Car {
-  static #MAX_PRICE = 50000;
-  // Change code below this line
-  checkPrice(price) {
-    if (price > MAX_PRICE) {
-      return "Error! Price exceeds the maximum";
-    } else {
-      return "Success! Price is within acceptable limits";
-    }
-  }
-  // Change code above this line
-  constructor({ price }) {
-    this.price = price;
-  }
-}
+// const student = {
+//   name: "Mango",
+//   email: "mango@mail.com",
+//   hwResults: [
+//     {
+//       topic: "HTML",
+//       success: true,
+//     },
+//     {
+//       topic: "CSS",
+//       success: true,
+//     },
+//     {
+//       topic: "JavaScript",
+//       success: false,
+//     },
+//   ],
+//   getName() {
+//     return this.name;
+//   },
+//   getEmail() {
+//     return this.email;
+//   },
+//   getHwResults() {
+//     return this.hwResults;
+//   },
+//   addHwResult(topic, success) {
+//     this.hwResults.push({ topic, success });
+//   },
+// };
 
-const audi = new Car({ price: 36000 });
-const bmw = new Car({ price: 64000 });
+// class Student {
+//   #name;
+//   #email;
+//   #hwResults = [];
+//   constructor({ name, email }) {
+//     this.#name = name;
+//     this.#email = email;
+//   }
 
-console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
-console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+//   getName() {
+//     return this.name;
+//   }
+//   getEmail() {
+//     return this.email;
+//   }
+//   getHwResults() {
+//     return this.hwResults;
+//   }
+//   addHwResult(topic, success) {
+//     this.#hwResults.push({ topic, success });
+//   }
+// }
+
+// const mango = new Student({ name: "mango", email: "email" });
+// console.log(mango);
+
+// console.log(mango.getName());
+// mango.addHwResults("CSS", true);
+// console.log(mango.getHwResults());
+
+// const Student = function (name, email) {
+//   const hwResults = [];
+//   // this.name = name;
+//   // this.email = email;
+//   this.getName = function () {
+//     return name;
+//   };
+//   this.getEmail = function () {
+//     return email;
+//   };
+
+//   this.getHwResults = function () {
+//     return hwResults;
+//   };
+
+//   this.addHwResult = function (topic, success) {
+//     hwRewsults.push({ topic, success });
+//   };
+// };
+
+// const poly = new Student("Poly", "email");
+// console.log(poly.getEmail());
+// console.log(poly.getName());
+// poly.addHwResult("HTML", true);
+// console.log(poly.getHwResults());
+
+// const listOfStudents = [
+//   {
+//     name: "John",
+//     email: "john@gmail.com",
+//   },
+//   {
+//     name: "Jane",
+//     email: "jane@gmail.com",
+//   },
+//   {
+//     name: "Jack",
+//     email: "jack@gmail.com",
+//   },
+//   {
+//     name: "Mary",
+//     email: "mary@gmail.com",
+//   },
+//   {
+//     name: "Robert",
+//     email: "robert@gmail.com",
+//   },
+//   {
+//     name: "Ann",
+//     email: "ann@gmail.com",
+//   },
+//   {
+//     name: "Brad",
+//     email: "brad@gmail.com",
+//   },
+//   {
+//     name: "Lisa",
+//     email: "lisa@gmail.com",
+//   },
+//   {
+//     name: "Matthew",
+//     email: "matthew@gmail.com",
+//   },
+//   {
+//     name: "Sandra",
+//     email: "sandra@gmail.com",
+//   },
+// ];
+
+// const FrontendSchool = function (students) {
+//   const superStudents = students.map(({ name, email }) => new Student(name, email));
+//   this.printStudentsList = function () {
+//     superStudents.forEach((student) =>
+//       console.log(`name: ${student.getName()}, email: ${student.getEmail()}`)
+//     );
+//   };
+// };
+
+// const school = new FrontendSchool(listOfStudents);
+// console.log(school);
+// school.printStudentsList();
+
+// // class FrontendSchool {
+// //   #students;
+// //   constructor(arr) {
+// //     this.#students = arr.map(({ name, email }) => new Student(name, email));
+// //   }
+// //   showStudents() {
+// //     this.#students.forEach((student) =>
+// //       console.log(`name: ${student.getName()}, email: ${student.getEmail()}`)
+// //     );
+// //   }
+// // }
+// // const school = new FrontendSchool(listOfStudents);
+// // school.showStudents();
+
+const message = document.querySelector("#message");
+console.log(message.value); // Default textarea message
+
+//===================================================
+const activeLink = document.querySelector(".btn.active");
+console.log(activeLink.href); // https://s.codepen.io/about
+
+//===================================================
+const image = document.querySelector(".image");
+console.log(image.src); // https://placeimg.com/640/480/animals
+image.src = "https://placeimg.com/640/480/tech";
