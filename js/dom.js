b0ed33d5d54792e249468fd00006b9d51dc22b00
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-02-09 18:06:53 +0200

    practic

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,29 @@
-const parent = document.querySelector("#parent");
+// const parent = document.querySelector("#parent");
 
-parent.addEventListener("click", (event) => {
-  console.log("event.target: ", event.target);
-  console.log("event.currentTarget: ", event.currentTarget);
-});
+// parent.addEventListener("click", (event) => {
+//   console.log("event.target: ", event.target);
+//   console.log("event.currentTarget: ", event.currentTarget);
+// });
+
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error("Set state error: ", error.message);
+  }
+};
+
+const load = (key) => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error("Get state error: ", error.message);
+  }
+};
+
+export default {
+  save,
+  load,
+};
