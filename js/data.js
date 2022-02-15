d3b1cf1d2958adf925b793e28c1afbd31ab2824c
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-02-15 07:28:03 +0200

    practic

--- js/data.js
+++ js/data.js
@@ -1,12 +1,106 @@
-const timer = {
-  start() {
-    const startTime = Date.now();
+// const delay = (ms) => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(ms);
+//     }, ms);
+//   });
+// };
 
-    setInterval(() => {
-      console.log(startTime);
-      console.log("идет время");
-    }, 1000);
-  },
+// const logger = (time) => console.log(`Resolved after ${time}ms`);
+
+// // Вызовы функции для проверки
+// delay(2000).then(logger); // Resolved after 2000ms
+// delay(1000).then(logger); // Resolved after 1000ms
+// delay(1500).then(logger); // Resolved after 1500ms
+
+// const users = [
+//   { name: "Mango", active: true },
+//   { name: "Poly", active: false },
+//   { name: "Ajax", active: true },
+//   { name: "Lux", active: false },
+// ];
+// console.table(users);
+
+// const toggleUserState = (allUsers, userName) => {
+//   const updatedUsers = allUsers.map((user) => {
+//     // user.name === userName ? { ...user, active: !user.active } : user
+//     if (user.name === userName) {
+//       return { ...user, active: !user.active };
+//     } else {
+//       return user;
+//     }
+//   });
+
+//   return new Promise((resolve) => {
+//     //
+//     resolve(updatedUsers);
+//   });
+//   //   return Promise.resolve(updatedUsers);
+// };
+
+// const logger = (array) => console.table(array);
+// // const logger = console.table;
+
+// /*
+//  * Сейчас работает так
+//  */
+// // toggleUserState(users, "Mango", logger);
+// // toggleUserState(users, "Lux", logger);
+
+// /*
+//  * Должно работать так
+//  */
+// toggleUserState(users, "Mango").then(logger);
+// toggleUserState(users, "Lux").then(logger);
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-timer.start();
+const makeTransaction = (transaction) => {
+  const delay = randomIntegerFromInterval(200, 500);
+
+  const canProcess = Math.random() > 0.3;
+
+  if (canProcess) {
+    onSuccess(transaction.id, delay);
+  } else {
+    onError(transaction.id);
+  }
+
+  return new Promise((resolve, reject) => {
+    if (canProcess) {
+      resolve([transaction.id, delay]);
+    } else {
+      reject(transaction.id);
+    }
+
+    setTimeout(() => {}, delay);
+  });
+};
+
+const logSuccess = ([id, time]) => {
+  console.log(`Transaction ${id} processed in ${time}ms`);
+};
+
+const logError = (id) => {
+  console.warn(`Error processing transaction ${id}. Please try again later.`);
+};
+
+/*
+ * Работает так
+ */
+// makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
+// makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
+// makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
+// makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
+/*
+ * Должно работать так
+ */
+makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
